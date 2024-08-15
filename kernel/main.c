007a45f6767d66134068e1e23db0b96f9be5a96a
JINGzmr <jingzmr@163.com>
2024-08-15 21:42:38 +0800

    feat:用汇编代码实现put_char函数的单个字符打印

--- kernel/main.c
+++ kernel/main.c
@@ -1,4 +1,17 @@
-int main(void){
+#include "print.h"
+void main(void)
+{
+    put_char('T');
+    put_char('i');
+    put_char('n');
+    put_char('y');
+    put_char('O');
+    put_char('S');
+    put_char('\n');
+    put_char('1');
+    put_char('2');
+    put_char('\b');
+    put_char('3');
     while(1);
-    return 0;
-} 
+    
+}
