124347a9207d6f0e64695f804e565a07b75b74cc
JINGzmr <jingzmr@163.com>
2024-08-17 12:13:58 +0800

    feat:实现字符串打印

--- kernel/main.c
+++ kernel/main.c
@@ -1,17 +1,5 @@
 #include "print.h"
-void main(void)
-{
-    put_char('T');
-    put_char('i');
-    put_char('n');
-    put_char('y');
-    put_char('O');
-    put_char('S');
-    put_char('\n');
-    put_char('1');
-    put_char('2');
-    put_char('\b');
-    put_char('3');
-    while(1);
-    
+void main(void) {
+   put_str("hello TinyOS\n");
+   while(1);
 }
