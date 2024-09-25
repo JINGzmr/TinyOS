2198f5d81672a26394a09f19dd629f0d325192b7
JINGzmr <jingzmr@163.com>
2024-09-25 10:37:14 +0800

    feat:实现系统调用malloc\free

--- userprog/syscall-init.c
+++ userprog/syscall-init.c
@@ -5,6 +5,7 @@
 #include "thread.h"
 #include "console.h"
 #include "string.h"
+#include "memory.h"
 
 #define syscall_nr 32 
 typedef void* syscall;
@@ -26,5 +27,7 @@ void syscall_init(void) {
 	put_str("syscall_init start\n");
 	syscall_table[SYS_GETPID] = sys_getpid;
 	syscall_table[SYS_WRITE] = sys_write;
+	syscall_table[SYS_MALLOC] = sys_malloc;
+   	syscall_table[SYS_FREE] = sys_free;
 	put_str("syscall_init done\n");
 }
